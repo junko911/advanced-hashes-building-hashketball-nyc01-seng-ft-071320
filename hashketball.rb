cf50eea179b48f235e9a78e4382c813c9f59d0d4
Junko Tahara <junkotahara911@gmail.com>
2020-07-10 17:26:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,34 +5,34 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [ {
         :player_name => "Alan Anderson",
-        :number => "",
-        :shoe => "",
-        :points => "",
-        :rebounds => "",
-        :assists => "",
-        :steals => "",
-        :blocks => "",
-        :slam_dunks => ""
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
       },{
         :player_name => "Reggie Evans",
-        :number => "",
-        :shoe => "",
-        :points => "",
-        :rebounds => "",
-        :assists => "",
-        :steals => "",
-        :blocks => "",
-        :slam_dunks => ""
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
       },{
         :player_name => "Brook Lopez",
-        :number => "",
-        :shoe => "",
-        :points => "",
-        :rebounds => "",
-        :assists => "",
-        :steals => "",
-        :blocks => "",
-        :slam_dunks => ""
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
       },{
         :player_name => "Mason Plumlee",
         :number => "",
@@ -44,7 +44,7 @@ def game_hash
         :blocks => "",
         :slam_dunks => ""
       },{
-        :player_name => "",
+        :player_name => "Jason Terry",
         :number => "",
         :shoe => "",
         :points => "",
