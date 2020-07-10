58c5ccd32930631bc45b628a6448d4d3f4b9706c
Junko Tahara <junkotahara911@gmail.com>
2020-07-10 17:29:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -80,15 +80,15 @@ def game_hash
         :blocks =>15,
         :slam_dunks => 10
       },{
-        :player_name => "Jeff Adrien",
-        :number => 4,
-        :shoe => 18,
-        :points => 10,
-        :rebounds => 1,
-        :assists => 1,
-        :steals => 2,
-        :blocks => 7,
-        :slam_dunks => 2
+        :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
       },{
         :player_name => "Jeff Adrien",
         :number => 4,
